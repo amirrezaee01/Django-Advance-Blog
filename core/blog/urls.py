43f0705115b35f0c5232cddabb1629ebdc505904
amirmohammadrezaee0090 <amirrezayi0101@gmail.com>
2025-01-26 11:21:42 +0330

    practice class base view

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -3,8 +3,13 @@ from blog.views import *
 from django.views.generic.base import RedirectView,TemplateView
 
 app_name = 'blog'
-
+ 
 urlpatterns = [
-    path('cbv-index', Indexview.as_view(), name= 'cbv-index'),
-    path('go-to-maktabkhooneh',RedirectToMaktab.as_view(), name='redirect-to-maktabkhooneh')
+    # path('cbv-index', Indexview.as_view(), name= 'cbv-index'),
+    # path('go-to-maktabkhooneh',RedirectToMaktab.as_view(), name='redirect-to-maktabkhooneh')
+    path('post/', PostListView.as_view(), name= 'post-list'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name= 'post-detail'),
+    path('post/create/', PostCreateView.as_view(), name= 'post-create'),
+    path('post/<int:pk>/edit/', PostEditView.as_view(), name= 'post-edit'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name= 'post-delete'),
 ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render 
 from django.views.generic.base import TemplateView,RedirectView
-from django.views.generic import ListView
+from django.views.generic import ListView,DeleteView,FormView,CreateView,UpdateView
 from blog.models import Post
+from blog.forms import PostForm
+from django.contrib.auth.mixins import LoginRequiredMixin,PermissionRequiredMixin
 from django.shortcuts import get_object_or_404
 
 
@@ -35,3 +37,53 @@ class RedirectToMaktab(RedirectView):
         print(post)
 
         return super().get_redirect_url(*args, **kwargs)
+
+class PostListView(LoginRequiredMixin,ListView):
+    
+    model = Post
+    context_object_name = 'posts'
+    # paginate_by = 2
+    ordering = 'id'
+    #permission_required = "blog.add_post"
+    # queryset = Post.objects.all()
+    # def get_queryset():
+    # posts = Post.object.filter(status=true)
+    # return posts
+    
+    
+class PostDetailView(DeleteView):
+    model = Post
+ 
+""""   
+#create post by FormView
+class PostCreateView(FormView):
+    template_name = 'contact.html'
+    form_class = PostForm
+    success_url = '/blog/post/'
+    
+    def form_valid(self, form):
+        # This method is called when valid form data has been POSTed.
+        # It should return an HttpResponse.
+        form.save()
+        return super().form_valid(form)
+"""
+class PostCreateView(CreateView):
+    model = Post
+    form_class = PostForm
+    success_url = '/blog/post/'
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+class PostEditView(UpdateView):
+    model = Post
+    form_class = PostForm
+    success_url = '/blog/post/'
+
+class PostDeleteView(DeleteView):
+    model = Post
+    success_url = '/blog/post/'
+    
+
+    
