a6ab6ba48d81f983a523dc0ccacd2fbc88b7a7c1
amirmohammadrezaee0090 <amirrezayi0101@gmail.com>
2025-03-09 01:17:45 +0330

    added simple serializer

--- core/blog/api/v1/urls.py
+++ core/blog/api/v1/urls.py
@@ -4,5 +4,6 @@ from blog.api.v1.views import *
 app_name = 'api-v1'
 
 urlpatterns = [
-    path('post/',postlist,name='post-list')
+    path('post/',postlist,name='post-list'),
+    path('post/<int:id>/',postDetail,name='post-detail')
 ]
--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -1,6 +1,14 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import PostSerializer
+from blog.models import Post
 
 @api_view()
 def postlist(request):
     return Response("its work")
+
+@api_view()
+def postDetail(request,id):
+    post = Post.objects.get(pk=id)
+    serializer = PostSerializer(post)
+    return Response(serializer.data)
