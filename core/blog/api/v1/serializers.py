2efdac8e9473625b4d993f751370c5a1bd8b879e
amirmohammadrezaee0090 <amirrezayi0101@gmail.com>
2025-03-11 00:21:06 +0330

    added  API crud FBV  and  login to api

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
+from ...models import Post
 
-class PostSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    content = serializers.CharField()
-    author = serializers.EmailField()
+
+#simple serializer like form that you have to full fill it by yourself
+# class PostSerializer(serializers.Serializer):
+#     id = serializers.IntegerField()
+#     title = serializers.CharField(max_length=255)
+#     content = serializers.CharField()
+#     author = serializers.EmailField()
+ 
+class PostSerializer(serializers.ModelSerializer):  
+    class Meta:  
+        model = Post  
+        fields = ['id', 'author','title', 'content', 'status', 'createed_date', 'published_date']
