d31e57653b11b5f0220265f03b7da818426c32d6
amirmohammadrezaee0090 <amirrezayi0101@gmail.com>
2025-03-12 01:23:09 +0330

    changed api from FBV to CBV

--- core/blog/api/v1/urls.py
+++ core/blog/api/v1/urls.py
@@ -4,6 +4,6 @@ from blog.api.v1.views import *
 app_name = 'api-v1'
 
 urlpatterns = [
-    path('post/',postlist,name='post-list'),
-    path('post/<int:id>/',postDetail,name='post-detail')
+    path('post/',PostList.as_view(),name='post-list'),
+    path('post/<int:id>/',PostDetail.as_view(),name='post-detail')
 ]
--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -1,4 +1,5 @@
 from rest_framework.decorators import api_view, permission_classes
+from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated,IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from .serializers import PostSerializer
@@ -6,33 +7,74 @@ from blog.models import Post
 from django.shortcuts import get_object_or_404
 
 
-@api_view(["GET","POST"])
-@permission_classes([IsAuthenticatedOrReadOnly])
-def postlist(request):
-    if request.method == "GET":
+class PostList(APIView):
+    """"getting a list of posts and creating new posts"""
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    serializer_class = PostSerializer
+    
+    def get(self,request):
+        """"retriveing a list of posts"""
         posts = Post.objects.filter(status=True)
         serializer = PostSerializer(posts,many=True)
         return Response(serializer.data)
-    elif request.method == "POST":
+    
+    def post(self,request):
+        """"creating a post with provided data"""
         serializer = PostSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
-         
-
-@api_view(["GET","PUT","DELETE"])
-@permission_classes([IsAuthenticatedOrReadOnly])
-def postDetail(request,id):
-    post = get_object_or_404(Post,pk=id,status=True)
-    if request.method == "GET":
-        serializer = PostSerializer(post)
+        
+class PostDetail(APIView):
+    """"getting detail of the post and edit plus removing it"""
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    serializer_class = PostSerializer
+    
+    def get(self,request,id):
+        """"retriveing the post data"""
+        post = get_object_or_404(Post,pk=id,status=True)
+        serializer = self.serializer_class(post)
         return Response(serializer.data)
-    elif request.method == "PUT":
+    
+    def put(self,request,id):
+        """"editing the post data"""
+        post = get_object_or_404(Post,pk=id,status=True)
         serializer = PostSerializer(post,data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
-    elif request.method == "DELETE":
+    def delete(self,request,id):
+        post = get_object_or_404(Post,pk=id,status=True)
         post.delete()
         return Response({"Detail":"post has been successfully removed"})
+    
+""""it is api with cbv"""
+# @api_view(["GET","POST"])
+# @permission_classes([IsAuthenticatedOrReadOnly])
+# def postlist(request):
+#     if request.method == "GET":
+#         posts = Post.objects.filter(status=True)
+#         serializer = PostSerializer(posts,many=True)
+#         return Response(serializer.data)
+#     elif request.method == "POST":
+#         serializer = PostSerializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data)
 
+#api for post detail with fbv  
+# @api_view(["GET","PUT","DELETE"])
+# @permission_classes([IsAuthenticatedOrReadOnly])
+# def postDetail(request,id):
+#     post = get_object_or_404(Post,pk=id,status=True)
+#     if request.method == "GET":
+#         serializer = PostSerializer(post)
+#         return Response(serializer.data)
+#     elif request.method == "PUT":
+#         serializer = PostSerializer(post,data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data)
+#     elif request.method == "DELETE":
+#         post.delete()
+#         return Response({"Detail":"post has been successfully removed"})
