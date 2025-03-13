26abdad07d7bb7d6854f5c383e329d4d80fe5412
amirmohammadrezaee0090 <amirrezayi0101@gmail.com>
2025-03-14 01:02:57 +0330

    changed with VIEWSET

--- core/blog/api/v1/urls.py
+++ core/blog/api/v1/urls.py
@@ -4,7 +4,10 @@ from blog.api.v1.views import *
 app_name = 'api-v1'
 
 urlpatterns = [
-    path('post/',PostList.as_view(),name='post-list'),
-    path('post/<int:pk>/',PostDetail.as_view(),name='post-detail')
+    # path('post/',PostList.as_view(),name='post-list'),
+    # path('post/<int:pk>/',PostDetail.as_view(),name='post-detail')
+    path('post/',PostViewSet.as_view({"get":"list","post":"create"}),name='post-list'),
+    path('post/<int:pk>/',PostViewSet.as_view({"get":"retrieve","put":"update","patch":"partial_update","delete":"destroy"}),name='post-detail'),
+    
 ]
  
--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -3,7 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from rest_framework.generics import GenericAPIView,ListAPIView,ListCreateAPIView,RetrieveUpdateDestroyAPIView
-from rest_framework import mixins
+from rest_framework import viewsets
 from .serializers import PostSerializer
 from blog.models import Post
 from django.shortcuts import get_object_or_404
@@ -21,4 +21,32 @@ class PostDetail(RetrieveUpdateDestroyAPIView):
     serializer_class = PostSerializer
     queryset = Post.objects.filter(status=True)
     
- 
+#the same method with viewset
+class PostViewSet(viewsets.ViewSet):
+
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    serializer_class = PostSerializer
+    queryset = Post.objects.filter(status=True)
+    
+    
+    def list(self, request):
+        Serializer = self.serializer_class(self.queryset,many=True)
+        return Response (Serializer.data)
+    
+    def retrieve(self, request, pk=None):
+        post_object = get_object_or_404(self.queryset,pk=pk)
+        Serializer = self.serializer_class(post_object)
+        return Response(Serializer.data)
+        
+
+    def create(self, request):
+        pass
+
+    def update(self, request, pk=None):
+        pass
+
+    def partial_update(self, request, pk=None):
+        pass
+
+    def destroy(self, request, pk=None):
+        pass
