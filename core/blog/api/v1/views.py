f226081fbffd1b89d682fd6c2bfd1ac033f3a7d3
amirmohammadrezaee0090 <amirrezayi0101@gmail.com>
2025-03-13 14:50:46 +0330

    changed the method from api view to crud genetic views

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -1,50 +1,24 @@
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.views import APIView
-from rest_framework.permissions import IsAuthenticated,IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
+from rest_framework.generics import GenericAPIView,ListAPIView,ListCreateAPIView,RetrieveUpdateDestroyAPIView
+from rest_framework import mixins
 from .serializers import PostSerializer
 from blog.models import Post
 from django.shortcuts import get_object_or_404
 
-
-class PostList(APIView):
-    """"getting a list of posts and creating new posts"""
+class PostList(ListCreateAPIView):
+    """Getting a list of posts and creating new posts"""
     permission_classes = [IsAuthenticatedOrReadOnly]
     serializer_class = PostSerializer
-    
-    def get(self,request):
-        """"retriveing a list of posts"""
-        posts = Post.objects.filter(status=True)
-        serializer = PostSerializer(posts,many=True)
-        return Response(serializer.data)
-    
-    def post(self,request):
-        """"creating a post with provided data"""
-        serializer = PostSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
+    queryset = Post.objects.filter(status=True)
         
-class PostDetail(APIView):
+    
+class PostDetail(RetrieveUpdateDestroyAPIView):
     """"getting detail of the post and edit plus removing it"""
     permission_classes = [IsAuthenticatedOrReadOnly]
     serializer_class = PostSerializer
+    queryset = Post.objects.filter(status=True)
     
-    def get(self,request,id):
-        """"retriveing the post data"""
-        post = get_object_or_404(Post,pk=id,status=True)
-        serializer = self.serializer_class(post)
-        return Response(serializer.data)
-    
-    def put(self,request,id):
-        """"editing the post data"""
-        post = get_object_or_404(Post,pk=id,status=True)
-        serializer = PostSerializer(post,data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-    def delete(self,request,id):
-        post = get_object_or_404(Post,pk=id,status=True)
-        post.delete()
-        return Response({"Detail":"post has been successfully removed"})
-    
+ 
