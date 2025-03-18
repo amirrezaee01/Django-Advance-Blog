5d6c70a37b729b8a600ecd8d1893bad994b286cb
amirmohammadrezaee0090 <amirrezayi0101@gmail.com>
2025-03-18 14:21:07 +0330

    add absolute_url to api

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -3,9 +3,17 @@ from ...models import Post,Category
 
 
 class PostSerializer(serializers.ModelSerializer):  
+    snippet = serializers.ReadOnlyField(source='get_snippet')
+    relative_url = serializers.URLField(source='get_absolute_api_url',read_only=True)
+    absolute_url = serializers.SerializerMethodField()
+
     class Meta:  
         model = Post  
-        fields = ['id', 'author','title', 'content', 'status', 'createed_date', 'published_date']
+        fields = ['id', 'author','title', 'content','snippet', 'status','relative_url','absolute_url', 'createed_date', 'published_date']
+    
+    def get_absolute_url(self,obj):
+        request = self.context.get('request')
+        return request.build_absolute_uri(obj.pk)
        
 class CategorySerializer(serializers.ModelSerializer):  
     class Meta:  
