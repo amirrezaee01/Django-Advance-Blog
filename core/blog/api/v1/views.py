f43fa9a10bf5b7df6549bb6b1981c35a25faba1c
amirmohammadrezaee0090 <amirrezayi0101@gmail.com>
2025-04-10 14:12:03 +0330

    added filter for view
    + filterset_fields
    + search_fields
    + ordering_fields

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -8,6 +8,8 @@ from .serializers import PostSerializer,CategorySerializer
 from blog.models import Post,Category
 from django.shortcuts import get_object_or_404
 from .permissions import IsOwnerOrReadOnly
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter,OrderingFilter
 
 
 #the same method with ViewSet in CBV
@@ -16,8 +18,13 @@ class PostModelViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly]
     serializer_class = PostSerializer
     queryset = Post.objects.filter(status=True)
+    filter_backends = [DjangoFilterBackend,SearchFilter,OrderingFilter]
+    filterset_fields = ['category', 'author','status']
+    search_fields = ['title', 'content']
+    ordering_fields = ['published_date']
     
 class CategoryModelViewSet(viewsets.ModelViewSet):
+    
     permission_classes = [IsAuthenticatedOrReadOnly]
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
--- core/core/settings.py
+++ core/core/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts',
     'blog',
-    'rest_framework'
+    'rest_framework',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
